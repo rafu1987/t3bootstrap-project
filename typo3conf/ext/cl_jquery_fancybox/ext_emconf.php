cb426ad8486ecea45730b60ef293ad00a677ec25
Raphael Zschorsch <rafu1987@gmail.com>
2012-12-07 01:37:24 +0100

    Updated cl_jquery_fancybox

--- typo3conf/ext/cl_jquery_fancybox/ext_emconf.php
+++ typo3conf/ext/cl_jquery_fancybox/ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "cl_jquery_fancybox".
-#
-# Auto generated 05-11-2012 12:03
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cl_jquery_fancybox".
+ *
+ * Auto generated 07-12-2012 01:36
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'jQuery FancyBox',
 	'description' => 'jQuery FancyBox 1&2 for TYPO3 with many options',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '3.0.3',
+	'version' => '3.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:49:{s:12:"ext_icon.gif";s:4:"5e07";s:14:"ext_tables.php";s:4:"95fa";s:14:"ext_tables.sql";s:4:"801c";s:28:"ext_typoscript_constants.txt";s:4:"68b3";s:24:"ext_typoscript_setup.txt";s:4:"e1c0";s:16:"locallang_db.xml";s:4:"e441";s:14:"doc/manual.sxw";s:4:"854f";s:19:"fancybox1/blank.gif";s:4:"3254";s:25:"fancybox1/fancy_close.png";s:4:"6e28";s:27:"fancybox1/fancy_loading.png";s:4:"b1d5";s:28:"fancybox1/fancy_nav_left.png";s:4:"3f3e";s:29:"fancybox1/fancy_nav_right.png";s:4:"216e";s:28:"fancybox1/fancy_shadow_e.png";s:4:"fd4f";s:28:"fancybox1/fancy_shadow_n.png";s:4:"18cd";s:29:"fancybox1/fancy_shadow_ne.png";s:4:"63ad";s:29:"fancybox1/fancy_shadow_nw.png";s:4:"c820";s:28:"fancybox1/fancy_shadow_s.png";s:4:"9b9e";s:29:"fancybox1/fancy_shadow_se.png";s:4:"a8af";s:29:"fancybox1/fancy_shadow_sw.png";s:4:"f81c";s:28:"fancybox1/fancy_shadow_w.png";s:4:"59b0";s:30:"fancybox1/fancy_title_left.png";s:4:"1582";s:30:"fancybox1/fancy_title_main.png";s:4:"38da";s:30:"fancybox1/fancy_title_over.png";s:4:"b886";s:31:"fancybox1/fancy_title_right.png";s:4:"6cbe";s:24:"fancybox1/fancybox-x.png";s:4:"1686";s:24:"fancybox1/fancybox-y.png";s:4:"36a5";s:22:"fancybox1/fancybox.png";s:4:"11e5";s:35:"fancybox1/jquery.easing-1.3.pack.js";s:4:"def2";s:35:"fancybox1/jquery.fancybox-1.3.4.css";s:4:"4638";s:34:"fancybox1/jquery.fancybox-1.3.4.js";s:4:"c326";s:39:"fancybox1/jquery.fancybox-1.3.4.pack.js";s:4:"8bc3";s:41:"fancybox1/jquery.mousewheel-3.0.4.pack.js";s:4:"3b0a";s:19:"fancybox2/blank.gif";s:4:"3254";s:30:"fancybox2/fancybox_loading.gif";s:4:"c6f1";s:29:"fancybox2/fancybox_sprite.png";s:4:"61c6";s:29:"fancybox2/jquery.fancybox.css";s:4:"32b1";s:28:"fancybox2/jquery.fancybox.js";s:4:"161b";s:33:"fancybox2/jquery.fancybox.pack.js";s:4:"7d5f";s:41:"fancybox2/jquery.mousewheel-3.0.6.pack.js";s:4:"fde6";s:38:"fancybox2/helpers/fancybox_buttons.png";s:4:"9fe7";s:45:"fancybox2/helpers/jquery.fancybox-buttons.css";s:4:"5d99";s:44:"fancybox2/helpers/jquery.fancybox-buttons.js";s:4:"9513";s:42:"fancybox2/helpers/jquery.fancybox-media.js";s:4:"b431";s:44:"fancybox2/helpers/jquery.fancybox-thumbs.css";s:4:"84a2";s:43:"fancybox2/helpers/jquery.fancybox-thumbs.js";s:4:"6315";s:30:"static/fancybox1/constants.txt";s:4:"93b9";s:26:"static/fancybox1/setup.txt";s:4:"5473";s:30:"static/fancybox2/constants.txt";s:4:"5000";s:26:"static/fancybox2/setup.txt";s:4:"950b";}',
+	'_md5_values_when_last_written' => 'a:49:{s:12:"ext_icon.gif";s:4:"5e07";s:14:"ext_tables.php";s:4:"95fa";s:14:"ext_tables.sql";s:4:"801c";s:28:"ext_typoscript_constants.txt";s:4:"68b3";s:24:"ext_typoscript_setup.txt";s:4:"e1c0";s:16:"locallang_db.xml";s:4:"e441";s:14:"doc/manual.sxw";s:4:"854f";s:19:"fancybox1/blank.gif";s:4:"3254";s:25:"fancybox1/fancy_close.png";s:4:"6e28";s:27:"fancybox1/fancy_loading.png";s:4:"b1d5";s:28:"fancybox1/fancy_nav_left.png";s:4:"3f3e";s:29:"fancybox1/fancy_nav_right.png";s:4:"216e";s:28:"fancybox1/fancy_shadow_e.png";s:4:"fd4f";s:28:"fancybox1/fancy_shadow_n.png";s:4:"18cd";s:29:"fancybox1/fancy_shadow_ne.png";s:4:"63ad";s:29:"fancybox1/fancy_shadow_nw.png";s:4:"c820";s:28:"fancybox1/fancy_shadow_s.png";s:4:"9b9e";s:29:"fancybox1/fancy_shadow_se.png";s:4:"a8af";s:29:"fancybox1/fancy_shadow_sw.png";s:4:"f81c";s:28:"fancybox1/fancy_shadow_w.png";s:4:"59b0";s:30:"fancybox1/fancy_title_left.png";s:4:"1582";s:30:"fancybox1/fancy_title_main.png";s:4:"38da";s:30:"fancybox1/fancy_title_over.png";s:4:"b886";s:31:"fancybox1/fancy_title_right.png";s:4:"6cbe";s:24:"fancybox1/fancybox-x.png";s:4:"1686";s:24:"fancybox1/fancybox-y.png";s:4:"36a5";s:22:"fancybox1/fancybox.png";s:4:"11e5";s:35:"fancybox1/jquery.easing-1.3.pack.js";s:4:"def2";s:35:"fancybox1/jquery.fancybox-1.3.4.css";s:4:"4638";s:34:"fancybox1/jquery.fancybox-1.3.4.js";s:4:"c326";s:39:"fancybox1/jquery.fancybox-1.3.4.pack.js";s:4:"8bc3";s:41:"fancybox1/jquery.mousewheel-3.0.4.pack.js";s:4:"3b0a";s:19:"fancybox2/blank.gif";s:4:"3254";s:30:"fancybox2/fancybox_loading.gif";s:4:"c6f1";s:29:"fancybox2/fancybox_sprite.png";s:4:"61c6";s:29:"fancybox2/jquery.fancybox.css";s:4:"32b1";s:28:"fancybox2/jquery.fancybox.js";s:4:"161b";s:33:"fancybox2/jquery.fancybox.pack.js";s:4:"7d5f";s:41:"fancybox2/jquery.mousewheel-3.0.6.pack.js";s:4:"fde6";s:38:"fancybox2/helpers/fancybox_buttons.png";s:4:"9fe7";s:45:"fancybox2/helpers/jquery.fancybox-buttons.css";s:4:"5d99";s:44:"fancybox2/helpers/jquery.fancybox-buttons.js";s:4:"9513";s:42:"fancybox2/helpers/jquery.fancybox-media.js";s:4:"b431";s:44:"fancybox2/helpers/jquery.fancybox-thumbs.css";s:4:"84a2";s:43:"fancybox2/helpers/jquery.fancybox-thumbs.js";s:4:"6315";s:30:"static/fancybox1/constants.txt";s:4:"ccb6";s:26:"static/fancybox1/setup.txt";s:4:"5473";s:30:"static/fancybox2/constants.txt";s:4:"5000";s:26:"static/fancybox2/setup.txt";s:4:"950b";}',
 );
 
 ?>
