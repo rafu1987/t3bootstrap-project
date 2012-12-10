28681dd95e192dc1146504ecb003d524061b36ab
Raphael Zschorsch <rafu1987@gmail.com>
2012-12-10 14:26:26 +0100

    medbugherd

---
+++ typo3conf/ext/medbugherd/README.md
@@ -0,0 +1,5 @@
+# medbugherd
+
+medbugherd is a TYPO3 extension that includes the JavaScript code of BugHerd. All you have to do, is provide the project API Key via TypoScript.
+
+**Powered by [medialis.net UG (haftungsbeschränkt)](http://www.medialis.net)**
---
+++ typo3conf/ext/medbugherd/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "medbugherd".
+ *
+ * Auto generated 10-12-2012 14:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'BugHerd',
+	'description' => 'Includes the BugHerd JavaScript. All you have to do, is provide the API Key.',
+	'category' => 'fe',
+	'author' => 'Raphael Zschorsch',
+	'author_email' => 'zschorsch@medialis.net',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"a234";s:12:"ext_icon.gif";s:4:"1bdc";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"033b";s:20:"doc/wizard_form.html";s:4:"d88b";}',
+);
+
+?>
